7e150488e6b7390a9b05623672fabf93414c2bc7
Landon Turner <lturner@teamworks.com>
2020-04-16 18:45:10 -0400

    get ingredient list for cocktail

--- cocktails/cocktail-model.js
+++ cocktails/cocktail-model.js
@@ -10,7 +10,16 @@ function getRecipes(){
     return db('cocktails');
 }
 function getShoppingList(id){
-    return db('cocktails').where({id}).first();
+    // SELECT ingredients.name from cocktails
+    // join cocktail_ingredient on cocktail_ingredient.cocktailId = cocktails.id
+    // join ingredients on cocktail_ingredient.ingredientId = ingredients.id
+    // WHERE cocktail_ingredient.cocktailId =1
+    // ;
+    return db('cocktails')
+    .join('cocktail_ingredient as ci', 'ci.cocktailId', 'cocktails.id')
+    .join('ingredients as i', 'ci.ingredientId', 'i.id')
+    .where('cocktails.id', id)
+    .select('i.name');
 }
 function getInstructions(id){
     return db
--- cocktails/cocktailRouter.js
+++ cocktails/cocktailRouter.js
@@ -11,6 +11,24 @@ router.get('/', (req, res) =>{
     .catch(err => {
         res.status(500).json({message: 'Couldn\'t get cocktails'})
     })
+});
+
+router.get('/:id/list', (req, res) =>{
+    const { id } = req.params;
+    console.log(id)
+    db.getShoppingList(id)
+    .then(list => {
+        if(list){
+            res.status(200).json(list)
+        }
+        else {
+            res.status(404).json({message: 'list not found'})
+        }
+        
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Couldn\'t get cocktails'})
+    })
 })
 
 module.exports = router;
